ae15b4d9a05a2840b7983521471a8d82d5ecdbb4
Maxim Dankovtsev <max.dankow@gmail.com>
2015-04-16 11:18:22 +0300

    Start working.

--- main.c
+++ main.c
@@ -2,16 +2,6 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include "sqlite3.h"
-  
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
-{
-    int i;
-    for(i=0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
-}
 
 int main(int argc, char **argv)
 {
@@ -23,15 +13,11 @@ int main(int argc, char **argv)
 
     if( rc )
     {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-     sqlite3_close(db);
-      return(1);
+       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+       sqlite3_close(db);
+       return(1);
     }
-    rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-    if( rc!=SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    }
+
     sqlite3_close(db);
     return 0;
 }
